6b74779011c76d7e7e89c4618d77aaf750ecba51
Serafim Nenarokov <serafim.nenarokov@flant.com>
2017-04-30 23:00:03 +0200

    Splice leaders: multiple files processing

--- bin/disposable/find_splice_leaders.py
+++ bin/disposable/find_splice_leaders.py
@@ -4,6 +4,7 @@ import os
 import math
 import time
 import re
+import ntpath
 from Bio import SeqIO
 import matplotlib.pyplot as plt
 import progressbar
@@ -75,31 +76,33 @@ def deep_reverse_complement(rec):
   new_rec.description = rec.description
   return new_rec
 
-reads_path = sys.argv[1]
+reads_paths = sys.argv[1:]
 
-f_size = int(os.popen('wc -l %s' % reads_path).read().split()[0])
-bar = progressbar.ProgressBar(max_value=int(math.ceil(f_size/4.0)))
+for path in reads_paths:
+  f_size = int(os.popen('wc -l %s' % path).read().split()[0])
+  bar = progressbar.ProgressBar(max_value=int(math.ceil(f_size/4.0)))
 
-sl_stats = {}
-poly_t_stats = {}
+  sl_stats = {}
+  poly_t_stats = {}
 
-sl_outfile = open("results/reads_with_SL.fq", "w")
-pa_outfile = open("results/reads_with_poly_a.fq", "w")
+  outfile_name = ntpath.basename(path).split('.')[0]
+  sl_outfile = open("results/%s_SL.fq" % outfile_name, "w")
+  pa_outfile = open("results/%s_poly_a.fq" % outfile_name, "w")
 
-for i, record in enumerate(SeqIO.parse(reads_path, "fastq")):
-  process_sl(record, sl_stats, sl_outfile)
-  process_poly_a(record, poly_t_stats, pa_outfile)
-  bar.update(i+1)
+  for i, record in enumerate(SeqIO.parse(path, "fastq")):
+    process_sl(record, sl_stats, sl_outfile)
+    process_poly_a(record, poly_t_stats, pa_outfile)
+    bar.update(i+1)
 
-print sl_stats
-# plt.plot(sl_stats.keys(), sl_stats.values(), color='blue')
-# for x, y in sl_stats.iteritems():
-#   plt.annotate(y, xy=(x, y), xytext=(x, y+1000), fontsize=8, color='blue')
+  print sl_stats
+  # plt.plot(sl_stats.keys(), sl_stats.values(), color='blue')
+  # for x, y in sl_stats.iteritems():
+  #   plt.annotate(y, xy=(x, y), xytext=(x, y+1000), fontsize=8, color='blue')
 
-print poly_t_stats
-# plt.plot(poly_t_stats.keys(), poly_t_stats.values(), color='red')
-# for x, y in poly_t_stats.iteritems():
-#   plt.annotate(y, xy=(x, y), xytext=(x, y+5000), fontsize=8, color='red')
+  print poly_t_stats
+  # plt.plot(poly_t_stats.keys(), poly_t_stats.values(), color='red')
+  # for x, y in poly_t_stats.iteritems():
+  #   plt.annotate(y, xy=(x, y), xytext=(x, y+5000), fontsize=8, color='red')
 
-# plt.axis([0, 35, 0, 100000])
-# plt.show()
+  # plt.axis([0, 35, 0, 100000])
+  # plt.show()
