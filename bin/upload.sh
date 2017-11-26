bfb2779c11370b1b36adf879cf6581ea10445b1a
Serafim Nenarokov <serafim.nenarokov@gmail.com>
2017-11-26 21:57:14 +0100

    Adds uploading script

---
+++ bin/upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FOLDER=/media/4TB1/blastocrithidia/annotation/p57
+DATE=$(date +"%B_%d")
+FOLDER=$FOLDER/$DATE
+
+i=1
+while ssh trypka "[ -d $FOLDER ]"
+do
+  i=$((i+1))
+  if [ $i = 2 ]; then
+    FOLDER=${FOLDER}_${i}
+  else
+    FOLDER=$(echo $FOLDER | sed "s#[0-9]\+\$#$i#")
+  fi
+done
+
+ssh trypka "mkdir $FOLDER"
+scp results/annotator/*.{txt,gff} trypka:$FOLDER
